a608279d4f1f1c8f3bf57148c09e57aff2c84bec
ganghuang_wu <815600709@qq.com>
2020-11-03 14:49:08 +0800

    feat: 下载逻辑完成

--- amazonListener.py
+++ amazonListener.py
@@ -21,26 +21,25 @@
 
 ## 库引入
 import time
+from config.config import Config
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.common.action_chains import ActionChains # 鼠标悬停
+from selenium.webdriver.common.action_chains import ActionChains # 鼠标悬停
+from selenium.webdriver.support import expected_conditions # 尝试
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.webdriver.common.by import By
 # import requests
 # from requests.exceptions import RequestException
 # from bs4 import BeautifulSoup 解析html的
 
-## 常量定义
-__tencentDocumentLoginUrl__ = 'https://docs.qq.com/desktop'
-__onlineDocumentUrl__ = 'https://docs.qq.com/sheet/DUVF5dE9TQ1lXUlZk?tab=BB08J2'
-__qqUsername__ = ''
-__qqPassword__ = ''
-__downloadPath__ = 'C:/Users/Administrator/Downloads/amazonListener.xlsx'
+__config__ = Config()
 
 
 ## Part One 检测文件内容
 ### driver爬腾讯文档流程：登录腾讯文档-> 导出->解析->根据需求通知
-### 腾讯文档登录 qq方式(账号/密码)
-def login():
-    driver.get(__tencentDocumentLoginUrl__)
+### 腾讯文档登录 qq方式(账号/密码) 并下载对应的excel
+def login(wait):
+    driver.get(__config__.__tencentDocumentLoginUrl__)
     time.sleep(3) # 3s静止
     # Iframe嵌套需要控制调整
     driver.switch_to_frame(driver.find_element_by_id('login_frame'))
@@ -48,16 +47,28 @@ def login():
     driver.find_element_by_id('switcher_plogin').click()
     time.sleep(1) # 等1s
     # 账号密码填入
-    driver.find_element_by_id('u').send_keys(__qqUsername__)
-    driver.find_element_by_id('p').send_keys(__qqPassword__)
+    driver.find_element_by_id('u').send_keys(__config__.__qqUsername__)
+    driver.find_element_by_id('p').send_keys(__config__.__qqPassword__)
     driver.find_element_by_id('login_button').click()
-    time.sleep(5) # 5s后直接访问文档地址
-    driver.get(__onlineDocumentUrl__)
-    time.sleep(20) # 10s后开始导出，因腾讯文档需要同步
+    time.sleep(3) # 3s后直接访问文档地址
+    driver.get(__config__.__onlineDocumentUrl__)
+    time.sleep(10) # 10s后开始导出，因腾讯文档需要同步
     # 鼠标点击‘更多’图标，此时才有导出的dom结构
     driver.find_element_by_class_name('titlebar-icon-more').click()
-    # driver.find_element_by_partial_link_text('本地Excel表格').click()
-    time.sleep(100)
+    time.sleep(1)
+    lis=driver.find_elements_by_class_name('dui-menu-item')
+    for item in lis:
+        if item.text == '导出为':
+            ActionChains(driver).move_to_element(item).perform()
+            # export_excel_xPath='/html/body/div[28]/ul/div[1]/li'
+            export_excel_xPath='//li[contains(text(), ".xlsx")]'
+            driver.find_element_by_xpath(export_excel_xPath).click()
+            break
+### 解析xlsx
+def parseXLSX():
+### 删除本地的xlsx
+def deleteDownloadXlsx():
+    
 ## Part Two 定时任务检查文件
 
 
@@ -72,4 +83,4 @@ def main():
 # 登录跳转流程
 with webdriver.Chrome() as driver:
     wait = WebDriverWait(driver, 10)
-    login()
+    login(wait)
